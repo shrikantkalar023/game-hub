f266ee6dbef2f709c7961f1f78571cca19970423
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-06-06 22:34:26 +0530

    removed api key from codebase

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "0882df45d2de40eba723b402ab8e8a47",
+    key: import.meta.env.VITE_apiKey,
   },
 });
