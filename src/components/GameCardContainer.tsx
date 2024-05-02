a2025318619ae91d833d2ab946b42c0ecf391153
Shrikant Kalar <shrikantkalar023@gmail.com>
2024-05-02 23:48:51 +0530

    ADD: hover effect on game cards

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -7,7 +7,12 @@ interface Props {
 
 const GameCardContainer = ({ children }: Props) => {
   return (
-    <Box borderRadius={10} overflow="hidden">
+    <Box
+      borderRadius={10}
+      overflow="hidden"
+      transition={"transform 0.2s ease-in-out"}
+      _hover={{ transform: "scale(1.04)" }}
+    >
       {children}
     </Box>
   );
